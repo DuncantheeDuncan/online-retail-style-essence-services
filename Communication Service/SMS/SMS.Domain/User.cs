8ee5644c906c99c9960b782fff221291cffddaad
anotid-a-mubaiwa <arnoldmubaiwa99@gmail.com>
2025-03-14 19:20:47 +0200

    Last refactor

--- Communication Service/SMS/SMS.Domain/User.cs
+++ Communication Service/SMS/SMS.Domain/User.cs
@@ -5,6 +5,5 @@ public class User
     public string Name { get; set; }
     public string Surname { get; set; }
     public Contact Contact { get; set; }
-    public DateOnly DateOfBirth { get; set; }
     public Role Role { get; set; }
 }
