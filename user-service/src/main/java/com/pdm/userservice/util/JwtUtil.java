e61cc6f97fbc71682640e425d9f1a527769e320e
DuncantheeDuncan <mthembup69@gmail.com>
2025-03-19 16:01:35 +0200

    added extractClaim method

--- user-service/src/main/java/com/pdm/userservice/util/JwtUtil.java
+++ user-service/src/main/java/com/pdm/userservice/util/JwtUtil.java
@@ -1,6 +1,7 @@
 package com.pdm.userservice.util;
 
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Component;
 import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
 import java.util.Date;
+import java.util.function.Function;
 
 @Component
 public class JwtUtil {
@@ -24,5 +26,21 @@ public class JwtUtil {
                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60)) // 1 hour expiry
                 .signWith(SignatureAlgorithm.HS256, key)
                 .compact();
+    }
+
+    private <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = Jwts.parser()
+                .setSigningKey(key) // Ensure you use the correct signing key (it should be a SecretKey or a byte array)
+                .parseClaimsJws(token) // Correct method to parse the JWT and validate the signature
+                .getBody(); // Get the body (claims)
+        return claimsResolver.apply(claims);
+    }
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public String extractRole(String token) {
+        return extractClaim(token, claims -> claims.get("role", String.class));
     }
 }
