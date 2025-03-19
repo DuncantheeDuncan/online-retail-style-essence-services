aef0c237544350e7a31f5fd5e5e91c12cc72445b
DuncantheeDuncan <mthembup69@gmail.com>
2025-03-19 14:34:45 +0200

    generate jwt token

--- user-service/src/main/java/com/pdm/userservice/util/JwtUtil.java
+++ user-service/src/main/java/com/pdm/userservice/util/JwtUtil.java
@@ -1,7 +1,6 @@
 package com.pdm.userservice.util;
 
 
-import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
@@ -9,9 +8,7 @@ import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
-import java.util.Base64;
 import java.util.Date;
-import java.util.function.Function;
 
 @Component
 public class JwtUtil {
@@ -28,31 +25,4 @@ public class JwtUtil {
                 .signWith(SignatureAlgorithm.HS256, key)
                 .compact();
     }
-//
-//    public String extractUsername(String token) {
-//        return extractClaim(token, Claims::getSubject);
-//    }
-
-//    public String extractRole(String token) {
-//        return extractClaim(token, claims -> claims.get("role", String.class));
-//    }
-
-//    private <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
-//        final Claims claims = Jwts.parser()
-//                .setSigningKey(SECRET_KEY)
-//                .build()
-//                .parseSignedClaims(token)
-//                .getPayload();
-//
-//
-//        return claimsResolver.apply(claims);
-//    }
-
-//    public boolean validateToken(String token, String username) {
-//        return extractUsername(token).equals(username) && !isTokenExpired(token);
-//    }
-
-//    private boolean isTokenExpired(String token) {
-//        return extractClaim(token, Claims::getExpiration).before(new Date());
-//    }
 }
